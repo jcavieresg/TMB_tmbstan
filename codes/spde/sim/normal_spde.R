468332bb93d8e2fd57fbe2c3bafb0819a15ec1d0
Joaquin Cavieres <55828236+jcavieresg@users.noreply.github.com>
2024-03-07 10:08:59 +0100

    Update normal_spde.R

--- codes/spde/sim/normal_spde.R
+++ codes/spde/sim/normal_spde.R
@@ -32,7 +32,6 @@ run_tmb <- function(nloc){
   y = runif(nloc, 0, 10)
   coords = cbind(x, y)
   mesh <- fm_mesh_2d(loc = coords, cutoff = 0.5, max.edge = c(nloc*2.4, 5))
-  #mesh <- inla.mesh.create(loc = coords, refine = T)
   mesh$n
   plot(mesh, main = "")
   
@@ -97,17 +96,6 @@ obj5 <- run_tmb(nloc = 500)
 obj6 <- run_tmb(nloc = 600)
 
 
-plot(obj1[[7]])
-points(obj1[[4]]$s1, obj1[[4]]$s2)
-plot(obj2[[7]])
-points(obj2[[4]]$s1, obj2[[4]]$s2)
-plot(obj3[[7]])
-points(obj3[[4]]$s1, obj3[[4]]$s2)
-
-
-obj1[[7]]$n
-obj2[[7]]$n
-
 obj1[[2]]$par
 obj2[[2]]$par
 obj3[[2]]$par
@@ -120,8 +108,6 @@ obj6[[2]]$par
 #===========================================================================================
 #                                        Fit with tmbstan
 #===========================================================================================
-
-
 M = list()
 M[[1]] = list()
 M[[1]]$model = "spatial_n100"
@@ -152,9 +138,6 @@ M[[6]]$formula = obj6[[1]]
 #           Run the models
 #===========================================
 
-# lwr <- c(-Inf, -Inf, 0, 0, 0)
-# upr <- c(Inf, Inf, Inf, Inf, Inf)
-
 for (i in 1:length(M)){
   startTime <- Sys.time()
   print(paste("Running:  ", M[[i]]$model))
@@ -162,7 +145,6 @@ for (i in 1:length(M)){
                  chains= 3, open_progress = FALSE,
                  control = list(max_treedepth= 13,  adapt_delta = 0.95),
                  iter = 4500, warmup= 700, cores=no_cores,
-                 #lower = lwr, upper = upr, 
                  init = 'last.par.best', seed = 12345)
   endTime <- Sys.time()
   timeUsed = difftime(endTime, startTime, units='mins')
@@ -189,7 +171,6 @@ c_dark_highlight <- c("#7C0000")
 # M[[5]]$res <- readRDS('stan_spde_5.RDS')
 # M[[6]]$res <- readRDS('stan_spde_6.RDS')
 
-
 m1 <- readRDS('stan_spde_1.RDS')
 m2 <- readRDS('stan_spde_2.RDS')
 m3 <- readRDS('stan_spde_3.RDS')
@@ -203,9 +184,6 @@ mon3 = monitor(m3)
 mon4 = monitor(m4)
 mon5 = monitor(m5)
 mon6 = monitor(m6)
-max(mon6$Rhat)
-min(mon6$Bulk_ESS)
-min(mon6$Tail_ESS)
 
 
 sum1 = data.frame(mon1$mean, mon1$`25%`, mon1$`75%`)
@@ -233,20 +211,15 @@ sum6 = data.frame(mon6$mean, mon6$`25%`, mon6$`75%`)
 colnames(sum6) = c("mean", "25%", "75%")
 head(sum6)
 
-summary(M[[1]]$res)
-summary(M[[2]]$res)
-
-
 posterior_1 = as.matrix(M[[1]]$res)
 posterior_2 = as.matrix(M[[2]]$res)
 posterior_3 = as.matrix(M[[3]]$res)
 posterior_4 = as.matrix(M[[4]]$res)
 
 
-traceplot(M[[1]]$res, pars=names(obj1$par), inc_warmup=TRUE)
-traceplot(M[[2]]$res, pars=names(obj2$par), inc_warmup=TRUE)
-
-traceplot(fit, pars=names(obj1$par), inc_warmup=TRUE)
+#traceplot(M[[1]]$res, pars=names(obj1$par), inc_warmup=TRUE)
+#traceplot(M[[2]]$res, pars=names(obj2$par), inc_warmup=TRUE)
+#traceplot(fit, pars=names(obj1$par), inc_warmup=TRUE)
 
 
 
@@ -291,9 +264,7 @@ p1 <- ggplot(df1, aes(y_sim)) +
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 1") + 
-  # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 1", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP1", colour = "blue", size = 10)
 
 for (i in 2:ncol(df1)) {
   p1 <- p1 + stat_function(fun = dnorm, 
@@ -311,9 +282,9 @@ p2 <- ggplot(df2, aes(y_sim)) +
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 2") + 
+  # ggtitle("SP2") + 
   # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 2", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP2", colour = "blue", size = 10)
 
 for (i in 2:ncol(df2)) {
   p2 <- p2 + stat_function(fun = dnorm, 
@@ -331,9 +302,9 @@ p3 <- ggplot(df3, aes(y_sim)) +
         #axis.title.y = element_blank(),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 3") + 
+  # ggtitle("SP3") + 
   # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 3", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP3", colour = "blue", size = 10)
 
 for (i in 2:ncol(df3)) {
   p3 <- p3 + stat_function(fun = dnorm, 
@@ -349,9 +320,9 @@ p4 <- ggplot(df4, aes(y_sim)) +
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 4") + 
+  # ggtitle("SP4") + 
   # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 4", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP4", colour = "blue", size = 10)
 
 for (i in 2:ncol(df4)) {
   p4 <- p4 + stat_function(fun = dnorm, 
@@ -366,9 +337,9 @@ p5 <- ggplot(df5, aes(y_sim)) +
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 5") + 
+  # ggtitle("SP5") + 
   # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 5", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP5", colour = "blue", size = 10)
 
 for (i in 2:ncol(df5)) {
   p5 <- p5 + stat_function(fun = dnorm, 
@@ -383,9 +354,9 @@ p6 <- ggplot(df6, aes(y_sim)) +
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
         axis.text = element_text(size = 14)) +
-  # ggtitle("Grid 6") + 
+  # ggtitle("SP6") + 
   # theme(plot.title = element_text(size = 22, hjust = 0.5))
-  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "Grid 6", colour = "blue", size = 10)
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.1, label = "SP6", colour = "blue", size = 10)
 
 for (i in 2:ncol(df6)) {
   p6 <- p6 + stat_function(fun = dnorm, 
@@ -399,20 +370,6 @@ grid.arrange(p1, p2, p3, p4, p5, p6, ncol = 3,
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #load package
 require(MCMCvis)
 MCMCsummary(m1, round = 2)
@@ -421,155 +378,3 @@ MCMCsummary(m1, round = 2)
 pairs(m1, pars = c("logsigma_e", "logtau", "lp__"), las = 1) # below the diagonal
 pairs(m1, pars = c("logsigma_e", "logtau", "logkappa")) # below the diagonal
 
-
-pairs(m1, pars = c("sigma_e", "kappa", "lp__"), las = 1) # below the diagonal
-m1
-
-
-
-
-
-
-
-
-
-
-
-
-# s <- summary(fit_tmbstan, probs = c(0.25, 0.75))
-# s$summary  # all chaines merged
-# 
-# exp(s$summary[3:5, c(1,3)])
-# s$summary[1:2, c(1,3)]
-# 
-# 
-# # Extract posterior draws for later use
-posterior_1 <- as.array(m1)
-
-
-require(MCMCvis)
-library(bayesplot)
-mcmc_pairs(posterior_1, pars = c("logsigma_e","logkappa","logtau"),
-           off_diag_args = list(size = 0.75))
-
-mcmc_pairs(m1, pars = c("sigma_e","kappa","tau"),
-           off_diag_args = list(size = 0.75))
-
-
-library(gridExtra)
-library(tidyverse)
-traceplot(M[[1]]$res, pars=names(obj1$par), inc_warmup=TRUE) + 
-  theme(strip.text = ggplot2::element_text(size = 16, color = "black"),
-        axis.text.x = element_text(size=12), 
-        axis.text.y = element_text(size=12),
-        legend.title = element_text(size=18),
-        legend.text = element_text(size=14))
-
-#load package
-require(MCMCvis)
-MCMCsummary(fit_tmbstan, round = 2)
-pairs(fit_tmbstan, pars = c("logsigma", "logtau", "logkappa")) # below the diagonal
-
-
-
-
-
-grfhat = summary(obj4[[3]], "random")[,1]
-par_fixed = summary(obj4[[3]], "fixed")
-betahat = summary(obj4[[3]], "fixed")["beta0", 1]
-obj4[[4]]$grf_est = obj4[[5]]$A %*% grfhat
-
-
-scl_lims = range(c(obj4[[4]]$grf, as.numeric(obj4[[4]]$grf_est)))
-# truth
-g_true =  ggplot(obj4[[4]]) +
-  geom_tile(aes(x = s1, y = s2, fill = grf)) +
-  coord_equal() +
-  scale_fill_viridis_c(limits = scl_lims) +
-  theme(legend.title = element_text(size=16),
-        legend.text = element_text(size=12),
-        axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 14))
-
-# estimate
-g_est =  ggplot(obj4[[4]]) +
-  geom_tile(aes(x = s1, y = s2, fill = as.matrix(grf_est))) +
-  coord_equal() +
-  scale_fill_viridis_c(limits = scl_lims) +
-  theme(legend.title = element_text(size=16),
-        legend.text = element_text(size=12),
-        axis.text.x = element_text(size = 14),
-        axis.text.y = element_text(size = 14))
-
-grid.arrange(g_true, g_est, ncol = 1)
-
-
-# Simulating from the SIMULATE function
-mat_sim = matrix(data=NA, nrow=length(obj1[[1]]$simulate()$y_sim), ncol=100)
-mat_sim
-
-
-for(j in 1:ncol(mat_sim)){
-  for(i in 1:nrow(mat_sim)){
-    mat_sim[, j] = obj1[[1]]$simulate()$y_sim
-  }
-}
-mat_sim
-
-
-# Plot lognormal non spatial simulations
-hist(obj1[[4]]$y_sim, col = "gray90", prob = TRUE, main = "100 simulated samples", cex.main = 2, xlab = "", col.lab = 'blue', col.main="blue", cex.lab = 1.4, cex.axis = 1.2, ylim = c(0, 0.5),
-     xlim = c(0, 5))
-for (j in 1: ncol(mat_sim)){
-  lines(x = density(x = mat_sim[, j]),  lty="dotted", col="azure4", lwd=1)
-}
-#legend("topright", "A", bty = "n", text.col="blue", cex = 1.4)
-legend(6, 0.3, c("response", "simulations"), lwd=4, col=c("gray90", "azure4"), cex = 1.5)
-
-
-hist(obj3[[2]]$y_sim)
-hist(obj2[[1]]$simulate()$y_sim)
-
-
-
-
-
-rep  = obj4[[3]]
-
-rangeIndex = which(row.names(summary(rep,"report"))=="rho")
-fieldIndex = which(row.names(summary(rep,"report"))=="u")
-range = summary(rep,"report")[rangeIndex,]
-
-proj = inla.mesh.projector(obj4[[7]])
-latentFieldMAP = rep$par.random[names(rep$par.random)=="u"]/exp(rep$par.fixed[which(names(rep$par.fixed)=="logtau")])
-#x11()
-par(mfrow=c(3,2), mar = c(5, 4, 2.5, 0.5), oma = c(0.5, 0.5, 0.2, 0.2))
-image.plot(proj$x,proj$y, inla.mesh.project(proj, latentFieldMAP),col =  colorRampPalette(c("white","gold", "blue"))(12),
-           xlab = '', ylab = 'Latitude',
-           main = "Mean of the spatial random effect",
-           cex.lab = 1.6,cex.axis = 1.3, cex.main=1.4,
-           cex.sub= 1.1,
-           axis.args=list(cex.axis=1.3),
-           zlim = range(-9, 9))
-bnd <- inla.mesh.boundary(obj4[[7]])
-inter <- inla.mesh.interior(obj4[[7]])
-plot(obj4[[7]], draw.segments=FALSE, main = '', add = T, col = "black")
-#points(cbind(obj1[[4]]$s1,obj1[[4]]$s2) ,type = 'p',lwd = 2, pch = 19, cex = 1.2)
-legend(-74.01, -41.956, legend=c("Spatial Gamma model"),
-       col=c("NA"), cex=1.4, box.lty=1)
-#contour(proj$x, proj$y,inla.mesh.project(proj, latentFieldMAP) ,add = T,labcex  = 1,cex = 1)
-text(x = -73.9, y = -41.795, expression(bold("S"[5])), cex = 1.8, font=4, col = "black")
-text(x = -73.85, y = -41.725, expression(bold("S"[9])), cex = 1.8)
-text(x = -73.79, y = -41.82,  expression(bold("S"[3])), cex = 1.8,  col = "black", font =  2)
-text(x = -73.76, y = -41.71, expression(bold("S"[7])), cex = 1.8)
-text(x = -73.72, y = -41.835, expression(bold("S"[1])), cex = 1.8, col = "black", font = 2)
-text(x = -73.70, y = -41.74, expression(bold("S"[8])), cex = 1.8,  col = "black", font = 2)
-text(x = -73.66, y = -41.815, expression(bold("S"[2])), cex = 1.8)
-text(x = -73.64, y = -41.76, expression(bold("S"[6])), cex = 1.8, col = "black", font  = 2)
-
-text(x = -73.5, y = -41.86, expression(bold("S"[10])), cex = 1.8)
-
-text(x = -72.98, y = -41.64, expression(bold("S"[13])), cex = 1.8)
-text(x = -72.98, y = -41.74, expression(bold("S"[4])), cex = 1.8)
-text(x = -73, y = -41.82, expression(bold("S"[12])), cex = 1.8)
-text(x = -73.1, y = -41.93, expression(bold("S"[11])), cex = 1.8)
