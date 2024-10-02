c446849d61066f6bedd66e27a4f61ad9917ca184
Joaquin Cavieres <55828236+jcavieresg@users.noreply.github.com>
2024-10-02 12:31:03 +0200

    Update loo_for_spde_tps.R

--- r_plots/real/loo_for_spde_tps.R
+++ r_plots/real/loo_for_spde_tps.R
@@ -1,198 +1,166 @@
-# if (!require(devtools)) {
-#   install.packages("devtools")
-# }
-# devtools::install_github("stan-dev/loo", build_vignettes = FALSE)
-# 
+setwd("")
 
-rm(list = ls())
-setwd("C:/Users/Usuario/Desktop/semipar_TPS/tmbstan_real_model")
+library(pacman)
+pacman::p_load(TMB, INLA, dplyr, tmbstan, rstan, parallel, bayesplot, ggplot2, loo, gridExtra,
+               fitdistrplus)
 
-library(loo)
-library(dplyr)
-library(ggplot2)
-library(rstan)
-set.seed(12345)
+options(scipen=999)
 
-obj_tps  = readRDS("C:/Users/Usuario/Desktop/semipar_TPS/tmbstan_real_model/fit_tps_TMB.RDS")
-obj_TMB  = readRDS("C:/Users/Usuario/Desktop/Spatial/fits_TMB.RDS")
-obj_spde = obj_TMB[[4]]
+M1    = readRDS("M1/stan_gamma_spde.RDS")
+M2    = readRDS("M2/stan_sn_spde.RDS")
+M3    = readRDS("M3/stan_gamma_tps.RDS")
+M4    = readRDS("M4/stan_sn_tps.RDS")
 
-posterior_tps  = readRDS("C:/Users/Usuario/Desktop/semipar_TPS/tmbstan_real_model/posterior_tps_tmbstan.RDS")
-posteriors  = readRDS("C:/Users/Usuario/Desktop/Spatial/posteriors_tmbstan.RDS")
-posterior_spde = posteriors[[4]]
+mon1 = monitor(M1)
+mon2 = monitor(M2)
+mon3 = monitor(M3)
+mon4 = monitor(M4)
 
-#======================================================================================================
-#                                   load the model created in TMB
-#                                 dyn.load(dynlib("spatial_tmbstan"))
-#======================================================================================================
-
-
-data = read.csv("north2.csv", header = T)
-head(data, 3)
-
-#========================
-# loo for the tps model
-# dyn.load(dynlib("semipar_SNTPS_tmbstan"))
-set.seed(1000)
-n_1 = length(data$cpue)
-log_lik_1 <- matrix(NA, nrow=nrow(posterior_tps), ncol=n_1)
-
-for(i in 1:nrow(posterior_tps)){
-  r1 <- obj_tps$report(posterior_tps[i,-ncol(posterior_tps)])
-  log_lik_1[i,] <- r1$log_lik
-  }
-
-loo_1 = loo(log_lik_1, r_eff = NA)
-
-
-#=========================
-# loo for the spde model
-# dyn.load(dynlib("spatial_tmbstan")) # only if it is neccesary!
-n_2 = length(data$cpue)
-log_lik_2 <- matrix(NA, nrow=nrow(posterior_spde), ncol=n_2)
-
-for(i in 1:nrow(posterior_spde)){
-  r2 <- obj_spde$Obj$report(posterior_spde[i,-ncol(posterior_spde)])
-  log_lik_2[i,] <- r2$log_lik
-}
-
-loo_2 = loo(log_lik_2, r_eff = NA)
-#print(loo_2)
+options(max.print = 1000000)
 
 
 
 
+#========================================
+#       Gamma models (spde and tps)
+#========================================
 
+summary.df <- as.data.frame(summary(M1, 
+                                    pars=c("beta_year[1]", "beta_year[2]", 
+                                           "beta_year[3]", "beta_year[4]", "beta_year[5]","beta_year[6]", "beta_year[7]", 
+                                           "beta_year[8]", "beta_year[9]", "beta_year[10]", "beta_year[11]", "beta_year[12]", 
+                                           "beta_year[13]", "beta_year[14]", "beta_year[15]", "beta_year[16]", "beta_year[17]", 
+                                           "beta_year[18]", "beta_year[19]", "beta_year[20]", "beta_year[21]")))[, c(1, 5, 6, 7)]
 
-#================================================================
-#               loo comparision for the 6 models
-#================================================================
-comp <- loo_compare(loo_1, loo_2)
-print(comp, digits = 3)
+years <- 1996:2016
+beta0_1 <- summary(M1)$summary[1, 1] 
+index <- summary(M1)$summary[2:22, 1] 
 
-par(mfrow=c(1,2))
-plot(loo_2, main =  expression(bold('Gamma model')), cex.lab = 1.5)
-plot(loo_1, main =  expression(bold('Skew normal model')), xlab="")
+all.df <- cbind(summary.df, index, years) 
+colnames(all.df) <- make.names(colnames(all.df)) # to remove % in the col names
 
+head(all.df)
 
-khat <- pareto_k_values(loo_1)
-kdata <- data.frame(Data_point = seq_along(khat), khat = khat, bad = khat > 0.7)
-plot1 = ggplot(kdata, aes(x = Data_point, y = khat, label = Data_point)) + 
-  #geom_hline(yintercept = 0.7, size = 1.0, linetype = 2, color = "blue") +
-  geom_point(shape = 1) +
-  geom_hline(yintercept = 0.7, size = 1.0, linetype = 2, color = "blue") + 
-  theme(axis.title.x = element_text(size = 16),
-        axis.title.y = element_blank(),
-        legend.text = element_text(size = 14), 
-        axis.text = element_text(size = 14)) +
-  ggtitle("Skew normal ST model") + 
-  theme(plot.title = element_text(size = 22, hjust = 0.5))
-
-
-khat <- pareto_k_values(loo_2)
-kdata <- data.frame(Data_point = seq_along(khat), khat = khat)
-plot2 = ggplot(kdata, aes(x = Data_point, y = khat)) + 
-  #geom_point(data = subset(kdata, !bad), size = 1.0, shape = 1) + ylim(-0.25, 0.9) +
-  geom_point(shape = 1) +
-  geom_hline(yintercept = 0.7, size = 1.0, linetype = 2, color = "blue") +
+p1 <- ggplot(all.df, mapping = aes(x = years)) + theme_bw() +
+  geom_ribbon(aes(ymin = summary.25., ymax = summary.75.), fill = "azure3", alpha = 0.6) +
+  geom_line(mapping = aes(y = summary.50.)) +
+  geom_point(mapping = aes(y = summary.50.)) +
+  scale_x_continuous("", labels = as.character(years), breaks = years) + 
+  labs(x = "", y = "Index") + # theme_bw() + 
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.2, label = "M1", colour = "black", size = 7, fontface = "bold") +
   theme(axis.title.x = element_text(size = 16),
         axis.title.y = element_text(size = 16),
         legend.text = element_text(size = 14), 
-        axis.text = element_text(size = 14)) +
-  ggtitle("Gamma ST model") + 
+        axis.text = element_text(size = 14),
+        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
   theme(plot.title = element_text(size = 22, hjust = 0.5))
 
-library(gridExtra)
-grid.arrange(plot2, plot1, ncol = 2)
+p1 
+
+
+summary.df2 <- as.data.frame(summary(M2,pars=c("beta_year[1]", "beta_year[2]", 
+                                                              "beta_year[3]", "beta_year[4]", "beta_year[5]","beta_year[6]", "beta_year[7]", 
+                                                              "beta_year[8]", "beta_year[9]", "beta_year[10]", "beta_year[11]", "beta_year[12]", 
+                                                              "beta_year[13]", "beta_year[14]", "beta_year[15]", "beta_year[16]", "beta_year[17]", 
+                                                              "beta_year[18]", "beta_year[19]", "beta_year[20]", "beta_year[21]")))[, c(1, 5, 6, 7)]
+
+years2 <- 1996:2016
+beta0_2 <- summary(M2)$summary[1, 1] 
+index2 <- summary(M2)$summary[2:22, 1]
+
+all.df2 <- cbind(summary.df2, index2, years2) 
+colnames(all.df2) <- make.names(colnames(all.df2)) # to remove % in the col names
+
+head(all.df2)
+
+p2 <- ggplot(all.df2, mapping = aes(x = years2)) + theme_bw() +
+  geom_ribbon(aes(ymin = summary.25., ymax = summary.75.), fill = "azure3", alpha = 0.6) +
+  geom_line(mapping = aes(y = summary.50.)) +
+  geom_point(mapping = aes(y = summary.50.)) +
+  scale_x_continuous("", labels = as.character(years2), breaks = years2) + 
+  labs(x = "", y = "") +# theme_bw() + 
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.2, label = "M2", colour = "black", size = 7, fontface = "bold") +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        legend.text = element_text(size = 14), 
+        axis.text = element_text(size = 14),
+        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  theme(plot.title = element_text(size = 22, hjust = 0.5))
+p2
+
+
+summary.df3 <- as.data.frame(summary(M3,
+                                     pars=c("beta_year[1]", "beta_year[2]", 
+                                     "beta_year[3]", "beta_year[4]", "beta_year[5]","beta_year[6]", "beta_year[7]", 
+                                     "beta_year[8]", "beta_year[9]", "beta_year[10]", "beta_year[11]", "beta_year[12]", 
+                                     "beta_year[13]", "beta_year[14]", "beta_year[15]", "beta_year[16]", "beta_year[17]", 
+                                     "beta_year[18]", "beta_year[19]", "beta_year[20]", "beta_year[21]")))[, c(1, 5, 6, 7)]
+
+
+
+years3 <- 1996:2016
+index3 <- summary(M3)$summary[2:22, 1]
+
+all.df3 <- cbind(summary.df3, index3, years3) 
+colnames(all.df3) <- make.names(colnames(all.df3)) # to remove % in the col names
+
+head(all.df3)
+
+p3 <- ggplot(all.df3, mapping = aes(x = years3)) + theme_bw() +
+  geom_ribbon(aes(ymin = summary.25., ymax = summary.75.), fill = "azure3", alpha = 0.6) +
+  geom_line(mapping = aes(y = summary.50.)) +
+  geom_point(mapping = aes(y = index3)) +
+  #scale_y_continuous(limits = c(-2, 2), labels = scales::label_number(accuracy = 0)) +
+  scale_x_continuous("Years", labels = as.character(years3), breaks = years3) + 
+  labs(x = "Years", y = "Index") +# theme_bw() + 
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.2, label = "M3", colour = "black", size = 7, fontface = "bold") +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        legend.text = element_text(size = 14), 
+        axis.text = element_text(size = 14),
+        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  #ggtitle("M3") + 
+  theme(plot.title = element_text(size = 22, hjust = 0.5)) + 
+  scale_y_continuous(limits = c(-0.5, 0.8), labels = label_number(accuracy = 0.1))
+p3
 
 
 
 
-mcmc_areas(
-  posterior_tps, 
-  pars = c("beta0", "sigma", "alpha", "sigma"),
-  prob = 0.8, # 80% intervals
-  prob_outer = 0.99, # 99%
-  point_est = "mean"
-)
-
-
-library(loo)
-library(rstan)
-mon1 = monitor(posterior_tps)
-
-data.frame(posterior_tps[, 5])
-
-ggplot(pl, aes(Time)) + 
-  geom_line(aes(y=menle), colour="blue") + 
-  geom_ribbon(aes(ymin=menlelb, ymax=menleub), alpha=0.2)
+summary.df4 <- as.data.frame(summary(M4,
+                                     pars=c("beta_year[1]", "beta_year[2]", 
+                                     "beta_year[3]", "beta_year[4]", "beta_year[5]","beta_year[6]", "beta_year[7]", 
+                                     "beta_year[8]", "beta_year[9]", "beta_year[10]", "beta_year[11]", "beta_year[12]", 
+                                     "beta_year[13]", "beta_year[14]", "beta_year[15]", "beta_year[16]", "beta_year[17]", 
+                                     "beta_year[18]", "beta_year[19]", "beta_year[20]", "beta_year[21]")))[, c(1, 5, 6, 7)]
 
 
 
+years4 <- 1996:2016
+index4 <- summary(M4)$summary[2:22, 1]
 
+all.df4 <- cbind(summary.df4, index4, years4) 
+colnames(all.df4) <- make.names(colnames(all.df4)) # to remove % in the col names
 
-#======================================================================
-#      Additionaly comparitions for Gamma model (sensivity analysis)
-#======================================================================
+head(all.df4)
 
-posteriors  = readRDS("C:/Users/Usuario/Desktop/Spatial/posteriors_tmbstan.RDS")
-
-# posterior_tps = posteriors[[4]]
-# posterior_spde = posteriors[[2]]
-# posterior_3 = posteriors[[3]]
-posterior_4 = posteriors[[4]]
-
-
-
-# loo for log(tau) = model base (Spatial Gamma model)----> -3.78
-set.seed(1000)
-n_1 = length(data$cpue)
-log_lik_1 <- matrix(NA, nrow=nrow(posterior_4), ncol=n_1)
-
-for(i in 1:nrow(posterior_4)){
-  r1 <- obj4$Obj$report(posterior_4[i,-ncol(posterior_4)])
-  log_lik_1[i,] <- r1$log_lik
-}
-
-loo_1 = loo(log_lik_1, r_eff = NA)
+p4 <- ggplot(all.df4, mapping = aes(x = years4)) + theme_bw() +
+  geom_ribbon(aes(ymin = summary.25., ymax = summary.75.), fill = "azure3", alpha = 0.6) +
+  geom_line(mapping = aes(y = summary.50.)) +
+  geom_point(mapping = aes(y = index4)) +
+  scale_x_continuous("Years", labels = as.character(years4), breaks = years4) + 
+  labs(x = "Years", y = "") +# theme_bw() + 
+  annotate("text", x = -Inf, y = Inf, hjust = -0.1, vjust = 1.2, label = "M4", colour = "black", size = 7, fontface = "bold") +
+  theme(axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        legend.text = element_text(size = 14), 
+        axis.text = element_text(size = 14),
+        axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  theme(plot.title = element_text(size = 22, hjust = 0.5))
+p4
 
 
 
-
-# loo for log(tau) = model base (Spatial Gamma model)----> -0.975
-set.seed(1000)
-n_2 = length(data$cpue)
-log_lik_2 <- matrix(NA, nrow=nrow(posterior_4), ncol=n_2)
-
-for(i in 1:nrow(posterior_4)){
-  r2 <- obj4$Obj$report(posterior_4[i,-ncol(posterior_4)])
-  log_lik_2[i,] <- r2$log_lik
-}
-
-loo_2 = loo(log_lik_2, r_eff = NA)
-print(loo_2)
-
-
-# loo for log(tau) = model base (Spatial Gamma model)----> -15.12
-set.seed(1000)
-n_3 = length(data$cpue)
-log_lik_3 <- matrix(NA, nrow=nrow(posterior_4), ncol=n_3)
-
-for(i in 1:nrow(posterior_4)){
-  r3 <- obj4$Obj$report(posterior_4[i,-ncol(posterior_4)])
-  log_lik_3[i,] <- r3$log_lik
-}
-
-loo_3 = loo(log_lik_3, r_eff = NA)
-print(loo_3)
-
-
-
-
-#================================================================
-#               loo comparision for the 6 models
-#================================================================
-comp <- loo_compare(loo_1, loo_2, loo_3)
-print(comp, digits = 3)
-
-
+cairo_ps(filename='Fig14_p2.eps', width = 10, height = 7)
+grid.arrange(p1, p2, p3, p4, ncol = 2)
+dev.off()
